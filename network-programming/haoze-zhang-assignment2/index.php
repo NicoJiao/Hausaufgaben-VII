151aec3ba90b18a7dd787e5e4184076a72d2d709
David Zhang <david.zhang311@foxmail.com>
2016-11-17 16:58:12 +0100

    Add comments to the code

--- network-programming/haoze-zhang-assignment2/index.php
+++ network-programming/haoze-zhang-assignment2/index.php
@@ -1,16 +1,28 @@
 <?php
-ini_set('display_errors', 1);
+/*
+ * Internet programming assignment 2
+ * Author: Haoze Zhang
+ * Version: 17-11-2016
+ */
+// use session
 session_start();
+// display error in debugging
+ini_set('display_errors', 1);
+// variables required to insert into http
 $w1Color = $w2Color = $w3Color = "";
 $w1Error = $w2Error = $w3Error = "";
 $w1Value = $w2Value = $w3Value = "";
 $nameColor = $zipColor = $telColor = "";
 $nameError = $zipError = $telError = "";
 $nameValue = $zipValue = $telValue = "";
+// set step variable to identify page
 if (!isset($_SESSION['step']) || empty($_SESSION['step'])) {
 	$_SESSION["step"] = 0;
 }
-if (isset($_POST["wish1"])) {
+// check which form is being sent
+if (isset($_POST["wish1"])) { // page1 form
+	// validate inputs and save them as session variables
+	// if not valid, color corresponding td to red and prompt user error messages
 	$validated = true;
 	$validated &= validateWish($_POST["wish1"], $w1Color, $w1Error, $w1Value);
 	$validated &= validateWish($_POST["wish2"], $w2Color, $w2Error, $w2Value);
@@ -19,44 +31,53 @@ if (isset($_POST["wish1"])) {
 		$_SESSION["wish1"] = normalize($_POST["wish1"]);
 		$_SESSION["wish2"] = normalize($_POST["wish2"]);
 		$_SESSION["wish3"] = normalize($_POST["wish3"]);
-		$_SESSION["step"] = 1;
+		$_SESSION["step"] = 1; // display the next page (1)
 	}
-} elseif (isset($_POST["name"])) {
-	if (empty($_POST["name"])) {
+} elseif (isset($_POST["name"])) { // page2 form
+ 	// validate inputs and save them as session variables
+ 	// if not valid, color corresponding td to red and prompt user error messages
+	if (empty($_POST["name"])) { // name cannot be empty
 		$nameColor = "style='color: red;'";
 		$nameError = "Please leave your name";
-	} elseif (!preg_match("/^(\w+\s?)+$/", $_POST["name"])) {
+	} elseif (!preg_match("/^(\w+\s?)+$/", $_POST["name"])) { // name not preg match
 		$nameColor = "style='color: red;'";
 		$nameError = "Please use only letters and spaces";
-	} else {
+	} else { // name valid!
 		$nameValue = $_POST["name"];
 	}
-	if (empty($_POST["zip"])) {
+	if (empty($_POST["zip"])) { // zip empty
 		$zipColor = "style='color: red;'";
 		$zipError = "We need your address to reach you";
-	} elseif (!preg_match("/^[\w\s\d]+$/", $_POST["zip"])) {
+	} elseif (!preg_match("/^[\w\s\d]+$/", $_POST["zip"])) { // zip not preg match
 		$zipColor = "style='color: red;'";
 		$zipError = "Please use only digits, letters and space";
-	} else {
+	} else { // zip valid!
 		$zipValue = $_POST["zip"];
 	}
-	if (empty($_POST["tel"])) {
+	if (empty($_POST["tel"])) { // tel empty
 		$telColor = "style='color: red;'";
 		$telError = "We need your telephone to contact you";
-	} elseif (!preg_match("/^(\+)?(\d+[\s-]?)+$/", $_POST["tel"])) {
+	} elseif (!preg_match("/^(\+)?(\d+[\s-]?)+$/", $_POST["tel"])) { // tel not preg match
 		$telColor = "style='color: red;'";
 		$telError = "Phone number may start with + and have space and dash between numbers, nothing more";
-	} else {
+	} else { // tel valid!
 		$telValue = $_POST["tel"];
 	}
-	if (empty($nameError) && empty($zipError) && empty($telError)) {
+	if (empty($nameError) && empty($zipError) && empty($telError)) { // save form2 inputs as session variables
 		$_SESSION["name"] = normalize($_POST["name"]);
 		$_SESSION["zip"] = normalize($_POST["zip"]);
 		$_SESSION["tel"] = normalize($_POST["tel"]);
 		$_SESSION["step"] = 2;
 	}
 }
-
+/*
+ * validate wishes, cannot be empty and cannot contain digits
+ * $wish: the wish to validate
+ * &$color: css style in html
+ * &$error: error message prompt in html
+ * &$value: value saved in session
+ * return: boolean validated: true; not valid: false
+ */
 function validateWish($wish, &$color, &$error, &$value) {
 	$validated = true;
 	if (empty($wish)) {
@@ -68,11 +89,13 @@ function validateWish($wish, &$color, &$error, &$value) {
 		$error = "Please don't use numbers in a wish";
 		$validated = false;
 	} else {
-		$value = $wish;
+		$value = normalize($wish);
 	}
 	return $validated;
 }
-
+/*
+ * normalize data to prevent XSS
+ */
 function normalize($data) {
 	$data = trim($data);
 	$data = stripslashes($data);
@@ -92,6 +115,7 @@ function normalize($data) {
 <main>
 <article>
 	<h2><?php
+	// Different titles for each 3 pages
 	switch($_SESSION["step"]) {
 		case 0:
 			echo "My Wish List";
@@ -105,10 +129,11 @@ function normalize($data) {
 	}
 ?></h2>
 	<section>
+	<!-- form is sent back to the same script via POST method  -->
 	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 		<table>
 		<tr><td class="leftC" <?php echo $w1Color ?>>1. Wish:</td><td><?php 
-		
+		// Table 1 display as input in page 0, as content in other pages
 		switch($_SESSION["step"]) {
 			case 0:
 				echo "<input name='wish1' type='text' value='$w1Value'><td><span class='error'>$w1Error</span></td>";
@@ -140,6 +165,7 @@ function normalize($data) {
 		?></td></tr>
 		</table>
 		<table style="display: <?php 
+		// The second table which contains contact information is hidden in page 0
 		switch($_SESSION["step"]) {
 			case 0:
 				echo "none";
@@ -180,6 +206,7 @@ function normalize($data) {
 		}
 		?></td></tr>
 		</table>
+		<!-- Cancel button refreshes the page without transmiting a form -->
 		<input type="button" value="Cancel" onclick="window.location='index.php'">
 		<input type="submit" value="OK">
 	</form>
@@ -192,6 +219,8 @@ function normalize($data) {
 </footer>
 </body>
 <?php
+// The session variables are destroyed after the last page is generated
+// if enables user to make another wish list
 if ($_SESSION['step'] == 2) {
 	session_destroy();
 }
